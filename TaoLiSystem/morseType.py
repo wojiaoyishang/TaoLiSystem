c979611c820b38bf421c0c0a5383aa5a5e052312
我叫以赏 <422880152@qq.com>
2022-02-04 06:14:10 +0000

    支持库

--- TaoLiSystem/morseType.py
+++ TaoLiSystem/morseType.py
@@ -36,6 +36,13 @@ morseDict = {
 
 morseDict = dict(zip(morseDict.values(), morseDict.keys()))
 
+def morse2utf8(text):
+    try:
+        text = text.replace(".", "0").replace("-", "1")
+        return chr(int(text, 2))
+    except:
+        return ""
+
 class MorseType():
     def __init__(self):
         """启动程序"""
@@ -55,10 +62,10 @@ class MorseType():
         while True:
             oled.fill(0)
             oled.hline(0, 48, 128, 1)
-            if len(self.text) != 0:
-                oled.DispChar(self.text[:self.pos + 1] + "▎" + self.text[self.pos + 1:], 0, 0, 1, True)
-            else:
-                oled.DispChar("使用摩尔斯电码输入内容，A+B完成输入；更多说明查看说明书。", 0, 0, 1, True)
+
+            oled.DispChar(self.text[:self.pos + 1] + "▎" + self.text[self.pos + 1:], 0, 0, 1, True)
+
+                
             
             
             oled.DispChar("大" if self.capital else "小", 115, 50, 1, False)  
@@ -68,7 +75,7 @@ class MorseType():
             # 记录当前时间
             t = time.ticks_ms()
             while True:
-                time.sleep_ms(80)
+                time.sleep_ms(50)
                 if button_a.value() + button_b.value() == 0:
                     _ = self.text
                     del self
@@ -81,8 +88,8 @@ class MorseType():
                     self.temp += "-"
                     t = time.ticks_ms()  # 按了就重置时间
                     break
-                if time.ticks_ms() - t >= 800:  # 800ms 没按就转义
-                    _ = morseDict.get(self.temp, "")
+                if time.ticks_ms() - t >= 600:  # 600ms 没按就转义
+                    _ = morseDict.get(self.temp, morse2utf8(self.temp))
                     if not self.capital:
                         _ = _.lower()
                     if len(self.text) == 0:
@@ -106,3 +113,4 @@ class MorseType():
                     self.text = self.text[:self.pos] + _ + self.text[self.pos + 1:]
                     self.pos = max(self.pos - 1, 0)
                     break
+
