c0d342b0670f1f70040b4013fcccf3ed3b3184ca
我叫以赏 <422880152@qq.com>
2022-02-04 05:36:01 +0000

    主文件

---
+++ boot.py
@@ -0,0 +1,35 @@
+# boot.py 被定义为启动文件，可能是掌控板在一开始运行时就会最先调用这个文件吧
+# 嘿！欢迎使用陶丽系统，此系统由我叫以赏开发，献给高一信息老师陶丽，同时也献给皮卡丘和 OneShot 中的 Niko ！
+# 你正在阅读这段文字，意味着你正在阅读我留下的彩蛋，同时也是在阅读我的思想。
+# 在之后的注释中，也会出现这样的文字。不妨你可以找一找？
+# 这里是陶丽系统最开始的启动页面，讲真，我还拿不准到底该如何去写......
+
+# 导入 mpython 支持库
+from mpython import *
+import time  # 时间
+import _thread   # 导入线程模块
+# 引入图片文件
+import TaoLiSystem.image
+
+Loading = True # 控制是否继续加载
+
+def Loading_page():
+    boot_image = TaoLiSystem.image.boot_image()
+    i = 1  # 控制加载点的个数
+    while Loading:
+        time.sleep(0.5)
+        if not Loading:
+            break
+        oled.fill(0)
+        oled.bitmap(0, 8, boot_image, 48, 48, 1)
+        oled.DispChar("正在启动系统", 54, 23)
+        oled.DispChar("·" * i, 70, 36)
+        if not Loading:
+            break
+        oled.show()
+        i = 1 if i >= 6 else i + 1
+        
+    del boot_image
+    _thread.exit()
+
+_thread.start_new_thread( Loading_page, () )   # 启动加载线程
---
+++ main.py
@@ -0,0 +1,67 @@
+# main.py 被定义为主要文件，紧跟着 boot.py 之后，系统会自动调用
+# 怎么说呢...... 噩梦开始了......
+# 特别谢明：此页面由 Niko 测试 BUG，由 皮卡丘 初步试验。
+
+# 导入 mpython 支持库
+from mpython import *
+import time  # 时间
+import gc
+import _thread   # 导入线程模块
+
+# 导入陶丽库中的模块
+import TaoLiSystem.config as config
+from TaoLiSystem.ItemSelector import ItemSelector as ItemSelector
+
+# 陶丽库中的页面模块
+from TaoLiSystem.page.home import HomePage as HomePage  # 主页面
+from TaoLiSystem.page.setting import SettingPage as SettingPage  # 设置页面
+from TaoLiSystem.page.plugin import PluginPage as PluginPage  # 插件
+
+# 系统启动时的变量
+config.set_value("page", "home")  # 用于指示当前处于的页面
+config.set_value("wifiSSID", "")  # 当前连接的wifi
+
+
+# 这里开始进行加载配置
+import TaoLiSystem.loader
+
+# 给加载插件用的
+def loadPlugin(exec_code):
+    # 如果有插件加载那么也关闭加载
+    global Loading
+    Loading = False
+    oled.fill(0)
+    oled.show()
+    
+    exec(exec_code, globals(), locals())
+    del exec_code
+    gc.collect()
+
+gc.isenabled()  # 进行内存碎片自动收集
+TaoLiSystem.loader.main(loadPlugin)  # 开始正式加载
+
+# 加载在 boot.py 中被定义了，使用可以直接修改
+Loading = False
+
+# 初始化页面
+homePage = HomePage()
+settingPage = SettingPage()
+pluginPage = PluginPage(loadPlugin)
+
+
+# 先清个屏
+oled.fill(0)
+oled.show()
+time.sleep(1)
+
+
+def checkEvent():
+    while True:
+        homePage.checkEvent()
+
+# 查查事件
+_thread.start_new_thread( checkEvent, () )
+while True:
+    homePage.show()
+    settingPage.show()
+    pluginPage.show()
