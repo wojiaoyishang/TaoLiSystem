f7b365addcbfeb14329cb29ecde2e6307ca876e2
wojiaoyishang <422880152@qq.com>
2023-08-06 21:19:33 +0800

    更新

--- boot.py
+++ boot.py
@@ -36,7 +36,7 @@ if button_a.value() == 0 and button_b.value() == 0:  # 两个按钮同时按下
         oled.show()
 
 # 替换回文件（成功运行一次后，下一次换回文件）
-# 当用户按下B，main.py 变为 main.py.main；main.py.bak 变为 main.py 系统重启
+# 当用户按下A，main.py 变为 main.py.main；main.py.bak 变为 main.py 系统重启
 # 第一次重启时，将 main.py.main 变为 main.py.main_ 表示已经运行了备份的代码，并在下一次重启时换回代码
 # 下一次重启，程序发现有 main.py.main_ 便将 main.py 变为 main.py.bak；将 main.py.main_ 变为 main.py
 # 因为掌控板没有检查文件是否存在的功能，所以使用 try 语句来拦截报错。
